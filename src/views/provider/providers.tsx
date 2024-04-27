36c9a460e1fdaae1f14f42097ee682c27e45898f
Alexandre Lamberty <alexandrelamberty@gmail.com>
2024-04-27 12:46:18 +0200

    feat: new provider modal

--- src/views/provider/providers.tsx
+++ src/views/provider/providers.tsx
@@ -4,14 +4,25 @@ import { useDispatch, useSelector } from "react-redux";
 
 import PageHeader from "@/components/page-header";
 import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
 import { AppDispatch, RootState } from "@/store";
 import { fetchAllProviders } from "@/store/actions/provider.actions";
 import { providerColumns } from "@/views/provider/components/data-table-columns";
 import { ProviderDataTable } from "@/views/provider/components/provider-data-table";
 
+import { ProviderForm } from "./components/provider-form";
+
 export const Providers = () => {
     const dispatch = useDispatch<AppDispatch>();
     const providers = useSelector((state: RootState) => state.providers.providers);
+
     useEffect(() => {
         dispatch(fetchAllProviders());
     }, [dispatch]);
@@ -19,7 +30,23 @@ export const Providers = () => {
     return (
         <div className="flex-1 space-y-4 p-8 pt-6">
             <PageHeader title="Providers">
-                <Button variant="outline">New provider</Button>
+                <Dialog modal={true}>
+                    <DialogTrigger asChild>
+                        <Button variant="outline">New provider</Button>
+                    </DialogTrigger>
+                    <DialogContent
+                        className="sm:max-w-[600px]"
+                        onInteractOutside={(e) => {
+                            e.preventDefault();
+                        }}
+                    >
+                        <DialogHeader>
+                            <DialogTitle className="text-xl font-semibold">Provider</DialogTitle>
+                            <DialogDescription>Create a new provider. Click save when you're done.</DialogDescription>
+                        </DialogHeader>
+                        <ProviderForm />
+                    </DialogContent>
+                </Dialog>
             </PageHeader>
             <ProviderDataTable data={providers} columns={providerColumns} />
         </div>
